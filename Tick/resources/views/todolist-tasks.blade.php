7748c95c0c7a4575749902ec82c965b455a4e460
jacob <balanejacob1@gmail.com>
2021-05-19 17:16:44 +0800

    may 19A

--- Tick/resources/views/todolist-tasks.blade.php
+++ Tick/resources/views/todolist-tasks.blade.php
@@ -47,9 +47,128 @@
         <h3>List Name</h3>
     </div>
     <div id="todolist-tasks-content">
-        <a href="">
-            <div class="add-floating shadow">
-                <div><i class="bi bi-plus"></i></div>
+        <div id="tasks">
+            <div class="list-tasks">
+                <div class="list-of-tasks shadow">
+                    <div class="list-of-tasks-title">
+                        <p>Tasks</p>
+                    </div>
+                    <div class="list-of-tasks-content">
+                        
+                        @foreach ($tasks as $task)
+                            <div class="task" onclick="test('task-info-{{$task->tasks_id}}')">
+
+                                
+                                @if ($task->status == "done")
+                                    <div class="task-name done">
+                                        <form action="" method="post">
+                                            @csrf
+                                            <a onclick="this.parentNode.submit();">
+                                                <div>
+                                                    <i class="bi bi-check-circle-fill"></i>
+                                                </div>
+                                            </a>
+                                        </form>
+                                        <label for="">{{$task->task}}</label>
+                                    </div>
+                                    <div class="task-status-done">
+                                        <form action="">
+                                            @csrf
+                                            <a  onclick="this.parentNode.submit();"><i class="bi bi-trash2-fill"></i></a>
+                                        </form>
+                                    </div>
+                                @else
+                                    <div class="task-name unfinished">
+
+                                        <form action="{{route('todolist-markAsDone',$task->tasks_id)}}" method="post">
+
+                                            @csrf
+                                            <a onclick="this.parentNode.submit();">
+                                                <div>
+                                                    <i class="bi bi-dash-circle"></i>
+                                                    <i class="bi bi-check-circle-fill"></i>
+                                                </div>
+                                            </a>
+                                        </form>
+                                        <label for="">{{$task->task}}</label>
+                                    </div>
+                                    <div class="task-status-unfinished">
+                                        <div>
+                                            <form action="">
+                                                @csrf
+                                                <a onclick="this.parentNode.submit();"><i class="bi bi-trash2-fill"></i></a>
+                                            </form>
+                                        </div>
+                                    </div>
+                                @endif
+                                
+
+
+                            </div>
+                        @endforeach
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="list-side">
+                <div class="list-side-upper">
+
+                    <div class="list-status">
+                        <div class="shadow">
+                            list status
+                        </div>
+                    </div>
+                    
+                    @foreach ($tasks as $task)
+
+                        <div class="task-info task-info-{{$task->tasks_id}}">
+                            <div class="shadow">
+                                <div class="task-info-title">
+                                    <p>Task Name: <span>{{$task->task}}</span></p>
+                                </div>
+                                <div class="task-info-body">
+                                    <div>
+                                        <p>Subject: <span>{{$task->subject}}</span></p>
+                                    </div>
+                                    <div>
+                                        <p>Task Type: <span>{{$task->task_type}}</span></p>
+                                    </div>
+                                    <div>
+                                        <p>Due Date: <span>{{$task->due_date}}</span></p>
+                                    </div>
+                                    <div>
+                                        <p>Time: <span>{{$task->time}}</span></p>
+                                    </div>
+                                </div>
+                                <div class="task-info-footer">
+                                    <div>
+                                        <form action="">
+                                            <a href="">
+                                                <i class="bi bi-journal-arrow-up"></i> Update
+                                            </a>
+                                        </form>
+                                    </div>
+                                    <div>
+                                        <form action="">
+                                            <a href="">
+                                                <i class="bi bi-file-earmark-x"></i> Delete
+                                            </a>
+                                        </form>
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                        </div>
+
+                    @endforeach
+
+
+                </div>
+                <div class="new-task">
+                    <div class="shadow">
+                        add task
+                    </div>
+                </div>
             </div>
         </a>
         Content added here
