43cf7d2e885bed22f3aa989b797bb710d7a5e938
jacob <balanejacob1@gmail.com>
2021-05-19 17:56:29 +0800

    Update todolist-tasks.blade.php

--- Tick/resources/views/todolist-tasks.blade.php
+++ Tick/resources/views/todolist-tasks.blade.php
@@ -84,7 +84,8 @@
                                     <div class="task-status-done">
                                         <form action="">
                                             @csrf
-                                            <a  onclick="this.parentNode.submit();><i class="bi bi-trash2-fill"></i></a>
+                                            <a  onclick="this.parentNode.submit();"><i class="bi bi-trash2-fill"></i></a>
+
                                         </form>
                                     </div>
                                 @else
@@ -106,7 +107,9 @@
                                         <div>
                                             <form action="">
                                                 @csrf
-                                                <a onclick="this.parentNode.submit();><i class="bi bi-trash2-fill"></i></a>
+
+                                                <a onclick="this.parentNode.submit();"><i class="bi bi-trash2-fill"></i></a>
+
                                             </form>
                                         </div>
                                     </div>
